b157925e6362d15514bb9a0108e1bce1954975f1
Coby Tamayo <acobster@gmail.com>
2018-12-21 18:35:33 -0800

    Move handler generation back to Factory class

--- src/GrootScaffold/TokenHandler/TokenHandlerFactory.php
+++ src/GrootScaffold/TokenHandler/TokenHandlerFactory.php
@@ -13,23 +13,45 @@ namespace GrootScaffold\TokenHandler;
  * Creates TokenHandler objects
  */
 class TokenHandlerFactory {
-  public function create($token, array $options) : AbstractTokenHandler {
-    if (is_string($token)) {
-      return new TransparentTokenHandler($token);
+  public function generate_token_handlers(array $tokens, array $options) {
+    // Get a group of token chunks
+    $group = [];
+    while ($tokens) {
+      $token = array_shift($tokens);
+      $group[] = $token;
 
-    } elseif ($this->is_groot_comment_token($token)) {
-      return new GrootTokenHandler($token[1], $options);
+      if ($this->is_groot_hook_comment($token)) {
 
-    } elseif ($token[0] === T_NAMESPACE) {
-      return new NamespaceTokenHandler($token[1], $options);
+        // Groot hooks are always just single tokens
+        yield new GrootTokenHandler([$token], $options);
 
-    } else {
-      // any other array
-      return new TransparentTokenHandler($token[1]);
+      } elseif ($token[0] === T_NAMESPACE) {
+
+        // This token is the beginning of a namespace declaration.
+        // Group together all subsquent tokens in the declaration.
+        while ($tokens[0] !== ';') {
+          $group[] = array_shift($tokens);
+        }
+        // Get the last token in the declaration, which we know is just a ";"
+        $group[] = ';';
+
+        yield new NamespaceTokenHandler($group, $options);
+
+      } elseif (is_string($token)) {
+
+        // This token is a string.
+        // Group together all subsequent string tokens.
+        while (is_string($tokens[0])) {
+          $group[] = array_shift($tokens);
+        }
+
+        yield new TransparentTokenHandler($group, $options);
+
+      }
     }
   }
 
-  protected function is_groot_comment_token($token) {
+  protected function is_groot_hook_comment($token) {
     return is_array($token)
       && in_array($token[0], [T_COMMENT, T_DOC_COMMENT])
       && strpos($token[1], GrootTokenHandler::GROOT_TAG) !== false;
