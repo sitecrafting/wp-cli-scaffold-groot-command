285bddfb1963faa8f9250c226644d42f0d1d2e1b
Coby Tamayo <ctamayo@sitecrafting.com>
2019-01-11 13:28:14 -0800

    define path and ABSPATH, deal with warning in tests

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,5 +1,8 @@
 <?php
 
-define( 'ABSPATH', realpath(__DIR__ . '/../wp/') . '/' );
+if ( !defined('ABSPATH') && is_dir( __DIR__ . '/../wp/' ) ) {
+  // if ./wp doesn't exist, that means we haven't install wp core yet
+  define( 'ABSPATH', realpath(__DIR__ . '/../wp/') . '/' );
+}
 
 require __DIR__ . '/../vendor/autoload.php';
